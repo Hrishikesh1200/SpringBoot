adad6363f3d445693379d3b05ecf22583d5670ab
Hrishikesh1200 <hrishikeshlandge.eidiko@gmail.com>
2025-07-28 18:52:53 +0530

    Property & Constructor-arg in Spring.xml file

--- DemoSpring/src/main/java/org/example/App.java
+++ DemoSpring/src/main/java/org/example/App.java
@@ -9,12 +9,14 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        WiredStudent ws = applicationContext.getBean(WiredStudent.class);
+        WiredStudent ws = (WiredStudent) applicationContext.getBean("div");
         //ws.build();
-        ws.setAge(12);
-        ws .setName("Name using getter and setter because name and age are private in WiredStudentClass");
+      //  ws.setAge(12);
+       // ws .setName("Name using getter and setter because name and age are private in WiredStudentClass");
         System.out.println( ws.getAge() );
         System.out.println( ws.getName());
         System.out.println( "Hello World!" );
+        //ws.setSchool("GRV");
+        System.out.println( ws.getSchool());
     }
 }
--- DemoSpring/src/main/java/org/example/WiredStudent.java
+++ DemoSpring/src/main/java/org/example/WiredStudent.java
@@ -4,12 +4,27 @@ import java.sql.SQLOutput;
 
 public class WiredStudent {
 
-    String Address; //we can get this value direct because this is not private, So we don't need getter
+    String Address; //1)we can get this value direct because this is not private, So we don't need getter
                     // and setter to get value of address
 
-    private int age;//But when we declare int age, String Name as private we need getter and setter
+    private int age;//2)But when we declare int age, String Name as private we need getter and setter
     private String Name;//because these are private variable
 
+    //3)we can also add property in spring.xml file to get value of name and age like below
+    //<property name="age" value="1666"/>
+    //<property name="Name" value="Assigned value by Spring"/>
+    //this will get value from spring file
+
+    private String school;
+
+    public String getSchool() {return school;}
+
+    public void setSchool(String school) {this.school = school;}
+//Parameterized constructor - for getting value of School from spring.xml file
+    //using Constructor-arg
+    public WiredStudent(String school) {
+        this.school = school;
+    }
 
     public String getName() {return Name;}
 
