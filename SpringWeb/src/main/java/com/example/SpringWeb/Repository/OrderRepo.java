82c1affa028ffbbc4c0e2e422bdae631ba270249
hrishikesh1200 <hrishikeshlandge.eidiko@gmail.com>
2025-08-25 18:49:41 +0530

    message1

--- SpringWeb/src/main/java/com/example/SpringWeb/Repository/OrderRepo.java
+++ SpringWeb/src/main/java/com/example/SpringWeb/Repository/OrderRepo.java
@@ -4,6 +4,10 @@ import com.example.SpringWeb.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Integer> {
+    // Derived query method
+    List<Order> findByCustomerName(String customerName);
 }
--- SpringWeb/src/main/java/com/example/SpringWeb/SpringWebApplication.java
+++ SpringWeb/src/main/java/com/example/SpringWeb/SpringWebApplication.java
@@ -10,5 +10,6 @@ public class SpringWebApplication {
 
 		SpringApplication.run(SpringWebApplication.class, args);
 		System.out.println("Main Class");
+
 	}
 }
