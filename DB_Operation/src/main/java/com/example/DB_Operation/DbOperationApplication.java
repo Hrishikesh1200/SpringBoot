6dd1d842ca9a00f6618252c90c5e78714b54d2a1
hrishikesh1200 <hrishikeshlandge.eidiko@gmail.com>
2025-09-26 15:00:01 +0530

    commit on 2025-09-26 15:00:01

---
+++ DB_Operation/src/main/java/com/example/DB_Operation/DbOperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.DB_Operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbOperationApplication.class, args);
+	}
+
+}
---
+++ DB_Operation/src/test/java/com/example/DB_Operation/DbOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DB_Operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
